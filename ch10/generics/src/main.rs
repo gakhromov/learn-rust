2759b98757940c33e2066b36d40cd05463fca4b2
Grigory Khromov <gakhromov@yandex.ru>
2021-08-11 07:09:07 +0000

    feat: add one more option to fn in generics

--- ch10/generics/src/main.rs
+++ ch10/generics/src/main.rs
@@ -22,20 +22,43 @@ fn main() {
     println!("|p-O| = {}", p.distance_from_origin());
 
     let number_vec = vec![3, 6, 8, 2, 10];
-    println!("Max number in {:?} is {}", number_vec, largest(&number_vec));
+    println!(
+        "Max number in {:?} is {}",
+        number_vec,
+        largest_with_copiable(&number_vec)
+    );
 
     let number_lst = [13, 16, 28, 22, 10];
-    println!("Max number in {:?} is {}", number_lst, largest(&number_lst));
+    println!(
+        "Max number in {:?} is {}",
+        number_lst,
+        largest_with_ref(&number_lst)
+    );
 
     let char_lst = ['y', 'm', 'c', 'a', ' '];
-    println!("Max char in {:?} is {}", char_lst, largest(&char_lst));
+    println!(
+        "Max char in {:?} is {}",
+        char_lst,
+        largest_with_copiable(&char_lst)
+    );
 }
 
-fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+fn largest_with_copiable<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &number in list {
         if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
+
+fn largest_with_ref<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for number in list {
+        if *number > *largest {
             largest = number;
         }
     }
